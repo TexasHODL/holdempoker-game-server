a4d4795e07d98cd7a381f4633404630fc8cd748e
Lucas Cullen <lucas@bitcoinbrisbane.com.au>
2024-05-22 16:01:18 +1000

    Refactor redisCommands.js for consistency and readability

--- src/services/redis-db/redisCommands.js
+++ src/services/redis-db/redisCommands.js
@@ -8,37 +8,37 @@
  * Modified By: digvijay
  */
 
-const { redisClient } = require('./redisConnect');
+const { redisClient } = require("./redisConnect");
 
 const redisCommands = {};
 
 redisCommands.setOnlinePlayers = function setOnlinePlayers(playerId, cb) {
-    redisClient.sadd("online_players", playerId, function (err, result) {
-        console.log("here");
-        if (err) {
-            console.log("Error while setting online player", err);
-            return cb(err);
-        }
-        cb(null, result);
-    })
-}
+  redisClient.sadd("online_players", playerId, function (err, result) {
+    console.log("here");
+    if (err) {
+      console.log("Error while setting online player", err);
+      return cb(err);
+    }
+    cb(null, result);
+  });
+};
 
 redisCommands.getOnlinePlayers = function getOnlinePlayers(cb) {
-    redisClient.smembers("online_players", function (err, result) {
-        if (err) {
-            return cb(err)
-        }
-        cb(null, result);
-    })
-}
+  redisClient.smembers("online_players", function (err, result) {
+    if (err) {
+      return cb(err);
+    }
+    cb(null, result);
+  });
+};
 
 redisCommands.removePlayerOnline = function removePlayerOnline(playerId, cb) {
-    redisClient.srem("online_players", playerId, function (err, result) {
-        if (err) {
-            return cb(err);
-        }
-        cb(null, result);
-    })
-}
+  redisClient.srem("online_players", playerId, function (err, result) {
+    if (err) {
+      return cb(err);
+    }
+    cb(null, result);
+  });
+};
 
 module.exports = redisCommands;
